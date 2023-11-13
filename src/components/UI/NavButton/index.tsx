2f08c460dc339d7ad3ffebcb26cd66c724cd0855
Clovis <clovissantannaa@gmail.com>
2023-11-13 19:26:52 -0300

    Added styles buttonContainer

--- src/components/UI/NavButton/index.tsx
+++ src/components/UI/NavButton/index.tsx
@@ -1,9 +1,10 @@
+"use client"
 import { useState } from 'react';
 import { TiThMenu } from "react-icons/ti";
 
 export default function NavButton(){
     const ShowMenu = useState(false);
-    const menuIcon = useState(true);
+    
     return(
         <button><TiThMenu width={16} height={16} color={"red"} /></button>
     )
